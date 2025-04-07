61d9ec7636cf14c4966f730e13eb3c184c337a0c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-07 09:48:35 -0300

    adicionando o laco de repetiçao for

---
+++ src/js/script.js
@@ -0,0 +1,142 @@
+// //DECLARARAÇÕES E VARIAVEIS
+
+// var nome ="fiap";
+// console.log(nome);
+
+// let idade = 19;
+// console.log(idade)
+
+// const sobrenome="Fiapinho";
+// console.log(sobrenome)
+
+// //declarando uma variavel indefinida
+// let nome1;
+// console.log(nome1)
+
+// //declarando uma variavel nula
+// let nome2=null;
+// console.log(nome2)
+
+// //TIPOS DE VARIAVEIS
+
+// let exemplo1 =10;
+// console.log(typeof exemplo1)
+
+// let exemplo2 ="Tecnologia";
+// console.log(typeof exemplo2)
+
+// let exemplo3 =true;
+// console.log(typeof exemplo3)
+
+// let exemplo4={};
+// console.log(typeof exemplo4)
+
+// let exemplo5 =[];
+// console.log(typeof exemplo5)
+
+// //CONVERSÕES
+
+// let numFloat =123.456;
+// console.log(parseInt(numFloat));
+
+// let numString ="123.456";
+// console.log(parseFloat(numString));
+
+// //METODOS
+
+// //length = verifica o tamanho da string
+// let frase ="O mundo da tecnologia";
+// console.log(frase.length);
+
+// //indexOf -retorna um trecho do texto
+
+// let texto="A programação indomável e sustentavel";
+// console.log(texto.indexOf(""))
+
+
+// //slice - retorna um trecho do texto com inicio e final
+
+// let info ="Processamento de ponta";
+// console.log(info.slice(16,25))
+
+//OPERADORES ARITIMÉTICOS
+
+// const num1=10;
+// const num2=20;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+
+// OPERADORES LÓGICOS
+
+const num3=20;
+const num4=30;
+
+console.log(num3 < num4);
+console.log(num3 > num4 && num4 < num3);
+console.log(num3 < num4 || num4 < num3); 
+
+//OPERADORES DE COMPARAÇÃO
+
+console.log(num3 == num4);
+console.log(num3 === num4);
+
+// estruturaa condicional
+
+let logado=="fiap"
+
+if(logado=="fiap"){
+console.log("é verdadeiro")
+}
+//if/else
+let usuario="teste"
+if(usuario =="test"){
+console.log("usuario correto")
+}else{
+    console.log("usuario errado")
+}
+// if encadeado / aninhado
+ let idade=15
+ if(idade <=15){
+    console.log("menor de idade")
+}else if(idade>15 && idade < 18){
+    console.log("nao é maior mas pode entrar")
+}else{
+    console.log("pode entrar e é maior")
+}
+// switch
+let time = "Corinthias"
+switch(time){
+    case"Corinthians":
+        console.log("melhor time")
+        break;
+    case "sao paulo": 
+    console.log("nao e o melhor time")
+        break;
+    case "palmeiras"
+        console.log("nao tem mundial")
+        break;
+    default:
+        console.log("nenhuma das opçoes")   
+}
+
+// ternario
+let valor=100;
+let resultado = valor == 100 ? "certo" : "errado"
+console.log(resultado)
+
+// terminario encadeado 
+let produto = "notebok"
+const desconto= true
+
+const preco= produto= "notebok" ? desconto ? 100: 200: produto ==="mouse" ? 30 :100 ;
+console.log(preco)
+// estrutura de reoetiçao
+
+// for
+
+for(let i=0; i<=10;i++){
+    console.log("o valor de I é >")
+}
